c850f7a117b92f725809b758ac2107cdad55ec39
laura <lauralekeu13@gmail.com>
2021-01-25 15:16:55 +0100

    Intégration et découpe du template

---
+++ resources/views/templates/partials/_scripts.blade.php
@@ -0,0 +1,12 @@
+{{--
+      ressources/views/templates/partials/_scripts.php
+--}}
+
+<!-- Javascripts -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
+
+<!-- Scrolling Nav JavaScript -->
+<script src="{{ asset('assets/js/jquery.easing.min.js')}}"></script>
+<script src="{{ asset('assets/js/scrolling-nav.js')}}"></script>
